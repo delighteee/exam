eb64e1de9c98d3c4b9828eb75afc10ed5f5a2735
Taiwo Tolulope Oyewumi <106971765+delighteee@users.noreply.github.com>
2022-11-05 06:42:43 +0100

    Hosted app on firebase

---
+++ src/config/index.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB_LuaSmck9hQEKvde9_e1hQwkR7biywuQ",
+  authDomain: "hauxano-7bd70.firebaseapp.com",
+  projectId: "hauxano-7bd70",
+  storageBucket: "hauxano-7bd70.appspot.com",
+  messagingSenderId: "222877271657",
+  appId: "1:222877271657:web:deb3b74faaa0a068dc44f8",
+  measurementId: "G-FD4ZW7W95V"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
